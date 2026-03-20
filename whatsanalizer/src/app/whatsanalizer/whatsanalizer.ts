267515a5756e571d9ee720514ef2a71a7ca415e1
Mateus Carvalho <mateus0000oliveira@gmail.com>
2026-03-20 14:15:24 -0300

    primeiro commit

---
+++ whatsanalizer/src/app/whatsanalizer/whatsanalizer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-whatsanalizer',
+  imports: [],
+  templateUrl: './whatsanalizer.html',
+  styleUrl: './whatsanalizer.scss',
+})
+export class Whatsanalizer {
+
+}
